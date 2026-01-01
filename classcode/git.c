5bebf781e93cb8a12a876bf9e3f66d903a6a364f
Bhavini <BHAVINI.22584@stu.upes.ac.in>
2026-01-01 13:00:55 +0530

    daily commit

--- classcode/git.c
+++ classcode/git.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main() {
-    printf("hello world");
+    printf(" ---------------- \n HAPPY NEW YEARRR!! 2026 ..... OKH LETE SEE WHAT ARE NEW CHAPTERS OF MY LIFE , ONE I CAN GARNTE YOU IS GET CLOSEUR TO BECOMING RICH\n ---------------- ");
     return 0;
 }
